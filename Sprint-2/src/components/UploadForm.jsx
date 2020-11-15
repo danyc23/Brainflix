2f31a87bdf4a5b9e6303b3160c7af33f949d0a82
Daniel Coulson <danycoulson@hotmail.com>
2020-11-14 16:44:07 -0800

    upload page ready

--- Sprint-2/src/components/UploadForm.jsx
+++ Sprint-2/src/components/UploadForm.jsx
@@ -3,29 +3,35 @@ function UploadForm() {
   return (
     <div className="upload-form">
       <h1 className="upload-form__title">Upload Video</h1>
-      <h3 className="upload-form__video__title">VIDEO THUMBNAIL</h3>
-      <img
-        className="upload-form__video"
-        src={UploadImage}
-        alt="Video Preview"
-      />
-      <h3 className="upload-form__input__title">TITLE YOUR VIDEO</h3>
-      <div className="upload-form__input__container">
-        <input
-          className="upload-form__input__name"
-          type="text"
-          placeholder="Add a title to your video"
-        ></input>
-      </div>
-      <h3 className="upload-form__input__desc__title">
-        ADD A VIDEO DESCRIPTION
-      </h3>
-      <div className="upload-form__input__container upload-form__input__container--desc">
-        <textarea
-          className="upload-form__input__desc"
-          type="text"
-          placeholder="Add a description of your video"
-        />
+      <div className="upload-form__desktop-wrapper">
+        <div>
+          <h3 className="upload-form__video__title">VIDEO THUMBNAIL</h3>
+          <img
+            className="upload-form__video"
+            src={UploadImage}
+            alt="Video Preview"
+          />
+        </div>
+        <div>
+          <h3 className="upload-form__input__title">TITLE YOUR VIDEO</h3>
+          <div className="upload-form__input__container">
+            <input
+              className="upload-form__input__name"
+              type="text"
+              placeholder="Add a title to your video"
+            ></input>
+          </div>
+          <h3 className="upload-form__input__desc__title">
+            ADD A VIDEO DESCRIPTION
+          </h3>
+          <div className="upload-form__input__container upload-form__input__container--desc">
+            <textarea
+              className="upload-form__input__desc"
+              type="text"
+              placeholder="Add a description of your video"
+            />
+          </div>
+        </div>
       </div>
       <div className="upload-form__button__container">
         <button type="reset" className="upload-form__cancel--top">
